6b37ded769b1d4e491d226ae6cb40a807063d690
jemmygao <jemmygao@tencent.com>
2024-09-24 18:15:29 +0800

    fix method of obtaining UserId

--- app/src/main/java/com/tencent/tcmpp/demo/ipcplugin/OpenDataIPC.java
+++ app/src/main/java/com/tencent/tcmpp/demo/ipcplugin/OpenDataIPC.java
@@ -2,7 +2,9 @@ package com.tencent.tcmpp.demo.ipcplugin;
 
 import android.os.Bundle;
 
-import com.tencent.tcmpp.demo.bean.UserInfo;
+import com.tencent.tcmpp.demo.TCMPPDemoApplication;
+import com.tencent.tcmpp.demo.open.login.Login;
+import com.tencent.tcmpp.demo.open.login.LoginApi;
 import com.tencent.tmf.mini.api.bean.IpcRequestEvent;
 import com.tencent.tmf.mini.api.callback.BaseIpcPlugin;
 import com.tencent.tmfmini.sdk.annotation.IpcEvent;
@@ -17,7 +19,8 @@ public class OpenDataIPC extends BaseIpcPlugin {
     @IpcEvent(OPEN_DATA_IPC_EVENT_GET_USER_ID)
     public void invoke(IpcRequestEvent ipcRequestEvent) {
         Bundle resp = new Bundle();
-        resp.putString("userId", UserInfo.getInstance().getUserId());
+        LoginApi.UserInfo userInfo = Login.g(TCMPPDemoApplication.sApp).getUserInfo();
+        resp.putString("userId", userInfo != null ? userInfo.userId : null);
         MiniAppProxy proxy = ProxyManager.get(MiniAppProxy.class);
         resp.putString("nickName", proxy.getNickName());
         resp.putString("avatarUrl", proxy.getAvatarUrl());
